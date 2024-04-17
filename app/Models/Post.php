cf8561090c25b71c4ffec333accc31e7fad87da6
fauzanmzlm <fauzanmazlam88@gmail.com>
2024-04-17 14:49:06 +0800

    Eloquent Accessor

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,4 +15,16 @@ class Post extends Model
         'title',
         'content',
     ];
+
+    /**
+     * image
+     *
+     * @return Attribute
+     */
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image),
+        );
+    }
 }
